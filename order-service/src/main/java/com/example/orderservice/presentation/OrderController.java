8f2cc8ac257b369a75c1eeeef81352e7f01fc89b
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-04-09 20:20:00 +0900

    feat: Order 컨트롤러 api endpoint 수정

--- order-service/src/main/java/com/example/orderservice/presentation/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/presentation/OrderController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
