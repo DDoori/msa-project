e91c003f76a650f3f8abf7e6bd070db5861557fd
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-04-09 16:31:57 +0900

    feat: product dto 작성

--- product-service/src/main/java/com/example/productservice/dto/ProductStockRequest.java
+++ product-service/src/main/java/com/example/productservice/dto/ProductStockRequest.java
@@ -1,4 +1,11 @@
 package com.example.productservice.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public class ProductStockRequest {
+
+    @Min(1)
+    @NotNull
+    private Integer stock;
 }
