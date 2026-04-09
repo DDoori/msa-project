579935fefd88677ef5a6f8f2ae5e2c93fcb37115
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-04-09 16:23:53 +0900

    feat: product repository 전체 조회, 이름으로 조회

--- product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
@@ -1,4 +1,14 @@
 package com.example.productservice.repository;
 
-public class ProductRepository {
+import com.example.productservice.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    List<Product> findAll();
+    Page<Product> findByName(String name,Pageable pageable);
 }
