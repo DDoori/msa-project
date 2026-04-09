f454f1647987db3f892328d6151991d68d70b375
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-04-09 16:31:57 +0900

    feat: product dto 작성

--- product-service/src/main/java/com/example/productservice/dto/ProductCreateRequest.java
+++ product-service/src/main/java/com/example/productservice/dto/ProductCreateRequest.java
@@ -1,4 +1,17 @@
 package com.example.productservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.UUID;
+
 public class ProductCreateRequest {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private UUID providerId;
+
+    @NotBlank
+    private int stock;
 }
--- product-service/src/main/java/com/example/productservice/dto/ProductResponse.java
+++ product-service/src/main/java/com/example/productservice/dto/ProductResponse.java
@@ -1,4 +1,24 @@
 package com.example.productservice.dto;
 
+import com.example.productservice.domain.Product;
+import lombok.Getter;
+import java.util.UUID;
+
+@Getter
 public class ProductResponse {
+    private UUID id;
+    private UUID providerId;
+    private String name;
+    private int stock;
+
+    private ProductResponse(Product product){
+        this.id = product.getId();
+        this.providerId = product.getProviderId();
+        this.name = product.getName();
+        this.stock = product.getStock();
+    }
+
+    public static ProductResponse from(Product product) {
+        return new ProductResponse(product);
+    }
 }
