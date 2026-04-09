aea591ab707c50c1a2e67fda868d11065089f1b9
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-04-09 18:47:46 +0900

    feat: Order Entity, enum 작성

---
+++ order-service/src/main/java/com/example/orderservice/domain/Order.java
@@ -0,0 +1,41 @@
+package com.example.orderservice.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="orders")
+@Getter
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private UUID userId;
+
+    @Column(name = "product_id", updatable = false, nullable = false)
+    private UUID productId;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Builder
+    public Order(UUID userId, UUID productId, int quantity) {
+        this.userId = userId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.status = OrderStatus.PENDING;
+    }
+}
