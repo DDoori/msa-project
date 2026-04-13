9830d5066aa28d817ea75220f44a563374a598f5
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-04-13 17:43:42 +0900

    feat: 슬랙웹훅 url 환경변수 처리

--- notification-service/src/main/java/com/example/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/example/notificationservice/NotificationServiceApplication.java
@@ -1,13 +1,17 @@
 package com.example.notificationservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
-
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("../")
+                .ignoreIfMissing()
+                .load();
+        System.setProperty("SLACK_WEBHOOK_URL", dotenv.get("SLACK_WEBHOOK_URL", ""));
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
-
 }
